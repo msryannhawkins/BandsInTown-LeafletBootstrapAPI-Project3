3ecbcc2ad29913dc8f7aae5f7aebe841262affed
dflan9696 <dflan9696@gmail.com>
2023-07-19 21:46:45 -0500

    added map and fixed style.css also changed mongoDb refresenses in the code

--- Project 3/app.py
+++ Project 3/app.py
@@ -1,17 +1,23 @@
 from flask import Flask, render_template, jsonify, request
 # Import dependencies
-from pymongo import MongoClient
+import pymongo
 from pprint import pprint
 import json
 
-Mongo = MongoClient()
-db = Mongo["Project3_BandsInTown"]
-
 app = Flask(__name__)
+
+# Create connection variable
+conn = 'mongodb://localhost:27017'
+
+# Pass connection to the pymongo instance.
+client = pymongo.MongoClient(conn)
+
+db = client.Project3_BandsInTown
+
 
 #database for now; using only Tswift as example
 api = "https://rest.bandsintown.com/artists/Taylor%20Swift/events?app_id=23bc806cf4fe0991e7a90f97a8e63576&date=all"
-
+apiDrake = "https://rest.bandsintown.com/artists/Drake/events?app_id=23bc806cf4fe0991e7a90f97a8e63576&date=all"
 
 @app.route("/")
 def page():
@@ -24,6 +30,12 @@ def TSFunct():
         del t["_id"]
     return jsonify(TS_data)
 
+@app.route("/data/drake")
+def drakeFunct():
+    drake_data = list(db["Drake"].find())
+    for d in drake_data:
+        del d["_id"]
+    return jsonify(drake_data)
 
 
 if __name__ == "__main__":
