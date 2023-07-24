d395f36e06fcd8b688797b54f507b4c7c04f2566
dflan9696 <dflan9696@gmail.com>
2023-07-24 18:43:44 -0500

    bind pop up and added event listener to drop down menus

--- Project 3/static/js/example.js
+++ Project 3/static/js/example.js
@@ -22,20 +22,63 @@ L.tileLayer('https://{s}.tile.openstreetmap.org/{z}/{x}/{y}.png', {
     attribution: '&copy; <a href="https://www.openstreetmap.org/copyright">OpenStreetMap</a> contributors'
 }).addTo(myMap);
 
-var artist = document.getElementById("dd1mob1");
-var event = document.getElementById("dd2mob1");
+let artist = document.getElementById("dd1mob1");
+artist.addEventListener('change', onChangeArtist);
+let event = document.getElementById("dd2mob1");
+event.addEventListener('change', onChangeEvent);
 
 function onChangeArtist() {
-  var valueArtist = artist.value;
+  let valueArtist = artist.value;
   console.log(valueArtist);
 }
 artist.onchange = onChangeArtist;
 onChangeArtist();
 
 function onChangeEvent() {
-    var valueEvent = event.value;
+    let valueEvent = event.value;
     console.log(valueEvent);
 }
 event.onchange = onChangeEvent;
 onChangeEvent();
-  
+
+let artistURL = "https://rest.bandsintown.com/artists/"+onChangeArtist()+"/events?app_id=23bc806cf4fe0991e7a90f97a8e63576&date="+onChangeEvent();
+console.log(artistURL);
+
+
+
+d3.json(url).then(function (data) {
+    locCoord = []
+
+    for (let i = 0; i < data.length; i++) {
+
+        //convert lat and long into int.
+        let lat = (parseInt(data[i].venue.latitude));
+        let long = (parseInt(data[i].venue.longitude));
+        let city = data[i].venue.city;
+        if (
+            lat && long && city
+        ) {
+            let color = "#29cc5b";
+            let coord = [
+                //lat
+                lat,
+                //long
+                long
+            ];
+
+            let radius = 1000; // Adjust the radius as needed
+
+            L.circle(coord, {
+                fillOpacity: 0.75,
+                color: color,
+                radius: radius, // Add the radius parameter
+            })
+            .bindPopup(
+                "City: " + data[i].venue.city
+                + "<br> Venue: " + data[i].venue.name
+                + "<br> Date: " + data[i].datetime
+                + "</br><a href=" + data[i].url + "> Website </a>"
+            ).addTo(myMap);
+        }
+    }
+});
