c7430681e7f9cba56c8ba54211e973b595d1531d
dflan9696 <dflan9696@gmail.com>
2023-07-24 22:12:11 -0500

    added stuff

--- Project 3/static/js/example.js
+++ Project 3/static/js/example.js
@@ -22,63 +22,85 @@ L.tileLayer('https://{s}.tile.openstreetmap.org/{z}/{x}/{y}.png', {
     attribution: '&copy; <a href="https://www.openstreetmap.org/copyright">OpenStreetMap</a> contributors'
 }).addTo(myMap);
 
+
 let artist = document.getElementById("dd1mob1");
 artist.addEventListener('change', onChangeArtist);
 let event = document.getElementById("dd2mob1");
 event.addEventListener('change', onChangeEvent);
 
-function onChangeArtist() {
-  let valueArtist = artist.value;
-  console.log(valueArtist);
+function onChangeArtist(a) {
+    a = artist.options[artist.selectedIndex].value
+    console.log(a);
+    return a;
 }
-artist.onchange = onChangeArtist;
-onChangeArtist();
+artist.onchange = onChangeArtist();
 
-function onChangeEvent() {
-    let valueEvent = event.value;
-    console.log(valueEvent);
+function onChangeEvent(e) {
+    e = event.options[event.selectedIndex].value
+    console.log(e);
+    return e;
 }
-event.onchange = onChangeEvent;
-onChangeEvent();
+event.onchange = onChangeEvent();
 
-let artistURL = "https://rest.bandsintown.com/artists/"+onChangeArtist()+"/events?app_id=23bc806cf4fe0991e7a90f97a8e63576&date="+onChangeEvent();
-console.log(artistURL);
+let newUrl = "https://rest.bandsintown.com/artists/"+onChangeArtist()+"/events?app_id=23bc806cf4fe0991e7a90f97a8e63576&date="+onChangeEvent();
+console.log(newUrl);
+
+// function newURL(onChangeArtist, onChangeEvent) {
+//     console.log("https://rest.bandsintown.com/artists/"+onChangeArtist+"/events?app_id=23bc806cf4fe0991e7a90f97a8e63576&date="+onChangeEvent);
+//     return "https://rest.bandsintown.com/artists/"+onChangeArtist+"/events?app_id=23bc806cf4fe0991e7a90f97a8e63576&date="+onChangeEvent;
+// }
+// newUrl();
+
+//let artistURL = "https://rest.bandsintown.com/artists/"+onChangeArtist()+"/events?app_id=23bc806cf4fe0991e7a90f97a8e63576&date="+onChangeEvent();
+//console.log(artistURL);
 
 
 
+
+
+
+
+// Get the current date
+const currentDate = new Date();
 d3.json(url).then(function (data) {
-    locCoord = []
-
     for (let i = 0; i < data.length; i++) {
-
         //convert lat and long into int.
-        let lat = (parseInt(data[i].venue.latitude));
-        let long = (parseInt(data[i].venue.longitude));
+        let lat = parseInt(data[i].venue.latitude);
+        let long = parseInt(data[i].venue.longitude);
         let city = data[i].venue.city;
-        if (
-            lat && long && city
-        ) {
-            let color = "#29cc5b";
+        let eventDate = new Date(data[i].datetime);
+        if (lat && long && city) {
+            let color = eventDate > currentDate ? "green" : "red";
             let coord = [
                 //lat
                 lat,
                 //long
                 long
             ];
-
-            let radius = 1000; // Adjust the radius as needed
-
-            L.circle(coord, {
+            let radius = 20; // Adjust the initial radius as needed
+            let circle = L.circle(coord, {
                 fillOpacity: 0.75,
                 color: color,
-                radius: radius, // Add the radius parameter
-            })
-            .bindPopup(
-                "City: " + data[i].venue.city
+                radius: radius, // Add the initial radius parameter
+            });
+            circle.bindPopup(
+                "Location: " + data[i].venue.city +", "+ data[i].venue.region +", "+ data[i].venue.country
                 + "<br> Venue: " + data[i].venue.name
                 + "<br> Date: " + data[i].datetime
-                + "</br><a href=" + data[i].url + "> Website </a>"
+                + "</br>Purchase Tickets: <a href=" + data[i].url + "> Website </a>"
             ).addTo(myMap);
         }
     }
+    // Event listener to update circle marker radius upon zooming
+    myMap.on("zoomend", function () {
+        let zoomLevel = myMap.getZoom();
+        // Calculate the new radius based on the zoom level
+        let newRadius = radius * 2 * zoomLevel; // Adjust the multiplier as needed
+        // Iterate through the circles and update their radius
+        myMap.eachLayer(function (layer) {
+            if (layer instanceof L.Circle) {
+                layer.setRadius(newRadius);
+            }
+        });
+    });
 });
